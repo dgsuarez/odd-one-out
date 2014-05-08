612f54419059bc0fb956b3e80dbfad5a3ed5d09b
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-05-08 21:19:34 +0200

    start doimg stuff with quickcheck

--- src/OddOneOut.hs
+++ src/OddOneOut.hs
@@ -1,17 +1,23 @@
 module OddOneOut (
   {-filterExtensionLists,-}
   groupIntoExtensionLists,
-  ExtensionList
+  ExtensionList,
+  extension_group,
+  files,
+  makeExtensionList
 ) where
 
 
 data ExtensionList = ExtensionList {
   files :: [String],
-  extension :: String
+  extension_group :: [String]
 } deriving (Eq, Show)
 
 filterExtensionLists :: [ExtensionList] -> ExtensionList
 filterExtensionLists = undefined
 
-groupIntoExtensionLists :: [String] -> [String] -> [ExtensionList]
-groupIntoExtensionLists files extensions = map (\ext -> undefined) extensions
+groupIntoExtensionLists :: [String] -> [[String]] -> [ExtensionList]
+groupIntoExtensionLists files extension_groups = map (makeExtensionList files) extension_groups
+
+makeExtensionList :: [String] -> [String] -> ExtensionList
+makeExtensionList files extension_group = ExtensionList {extension_group = extension_group, files = files}
--- test/OddOneOutTest.hs
+++ test/OddOneOutTest.hs
@@ -1,6 +1,5 @@
 import OddOneOut
 
-import Data.Monoid
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
@@ -8,13 +7,26 @@ import Test.HUnit
 import Test.QuickCheck
 
 main :: IO ()
-main = defaultMainWithOpts
+main = defaultMain
        [ 
-        testProperty "empty" propEmpty
-       ] mempty
+        testGroup "groupIntoExtensionLists" [
+          testProperty "all empty" propEmpty,
+          testProperty "empty extensions" propEmptyExtensions,
+          testProperty "set ext" propSetExtensions,
+          testProperty "same or less files" propFilterFiles
+        ]
+       ] 
 
 emptyEL :: [ExtensionList]
 emptyEL = []
 
-propEmpty :: [String] -> [String] -> Property
-propEmpty xs ys = (null xs) ==> (null ys) ==> groupIntoExtensionLists ys xs == emptyEL
+propEmpty = groupIntoExtensionLists [] [] == emptyEL
+
+propEmptyExtensions xs = groupIntoExtensionLists xs [] == emptyEL
+
+propSetExtensions :: [String] -> [[String]] -> Property
+propSetExtensions files extension_groups = not (null extension_groups) ==> map extension_group (groupIntoExtensionLists files extension_groups) == extension_groups
+
+propFilterFiles :: [String] -> [String] -> Property
+propFilterFiles fs extensions = not (null fs) ==> not (null extensions) ==> (length . files) (makeExtensionList fs extensions) <= (length fs)
+
