8ec6727acf1e6980ed0eeab928953c33b032ef2f
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-06-01 12:07:03 +0200

    don't remember doing these changes

--- src/OddOneOut.hs
+++ src/OddOneOut.hs
@@ -1,23 +1,24 @@
 module OddOneOut (
-  {-filterExtensionLists,-}
-  groupIntoExtensionLists,
   ExtensionList,
+  groupIntoExtensionLists,
   extension_group,
   files,
   makeExtensionList
 ) where
 
+import Data.List
 
 data ExtensionList = ExtensionList {
   files :: [String],
   extension_group :: [String]
 } deriving (Eq, Show)
 
-filterExtensionLists :: [ExtensionList] -> ExtensionList
-filterExtensionLists = undefined
 
 groupIntoExtensionLists :: [String] -> [[String]] -> [ExtensionList]
 groupIntoExtensionLists files extension_groups = map (makeExtensionList files) extension_groups
 
 makeExtensionList :: [String] -> [String] -> ExtensionList
-makeExtensionList files extension_group = ExtensionList {extension_group = extension_group, files = files}
+makeExtensionList files extension_group = ExtensionList {extension_group = extension_group, files = (filterByExtension files $ head extension_group)}
+
+filterByExtension ::  [String] -> String -> [String]
+filterByExtension files ext = filter (isSuffixOf ext) files
