c1b3300805a586c5a1c00b16540b6bb35d7f6f40
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-06-04 19:06:56 +0200

    basic CLI

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,12 @@
+import System.Environment
+import Text.Regex (splitRegex, mkRegex)
+
 import OddOneOut
 
-main = undefined
+perform extensions input = oddOneOut (lines input) (map (splitRegex $ mkRegex ",") extensions)
+
+main = do
+  input <- getContents
+  args <- getArgs
+  mapM_ putStrLn $ perform args input
+
